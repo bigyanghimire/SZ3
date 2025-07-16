f3a9382bbd82f0692adff8799a5db1659483a11f
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-07-16 13:12:35 -0400

    changes

--- compile.sh
+++ compile.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+
 rm -fr build/
 rm -fr qsz/
 mkdir qsz
 mkdir build && cd build
+
 cmake -DCMAKE_INSTALL_PREFIX:PATH=/home/bigyang/amg-compress/SZ3/qsz ..
-make
+
+# Use all available CPU cores for faster build
+make -j$(nproc)
+
 make install
