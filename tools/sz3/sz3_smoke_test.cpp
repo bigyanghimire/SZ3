f715bc86e4cc361177c3cdfe7ffc703e747f5ed2
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-07-18 15:05:59 -0400

    changes

--- tools/sz3/sz3_smoke_test.cpp
+++ tools/sz3/sz3_smoke_test.cpp
@@ -86,7 +86,7 @@ int main(int argc, char **argv) {
     size_t cmpSize;
     // char *cmpData = SZ_compress(conf, input_data.data(), cmpSize);
 
-    char *cmpData = compress_data<double>(input_data.data(), size, cmpSize);
+    char *cmpData = compress_data<double>(input_data.data(), size+20, cmpSize);
 
     // auto dec_data_p = dec_data.data();
     // SZ_decompress(conf, cmpData, cmpSize, dec_data_p);
