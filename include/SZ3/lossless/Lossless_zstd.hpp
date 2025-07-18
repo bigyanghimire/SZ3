31546f22bf23e4bf9f813260edb95074f7e92a6d
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-07-18 15:09:49 -0400

    changes

--- include/SZ3/lossless/Lossless_zstd.hpp
+++ include/SZ3/lossless/Lossless_zstd.hpp
@@ -30,7 +30,7 @@ class Lossless_zstd : public concepts::LosslessInterface {
             fprintf(stderr, "%s\n", SZ_ERROR_COMP_BUFFER_NOT_LARGE_ENOUGH);
             throw std::length_error("lossless error");
         }
-        size_t dstLen = ZSTD_compress(dst, dstCap, src, srcLen, compression_level);
+        // size_t dstLen = ZSTD_compress(dst, dstCap, src, srcLen, compression_level);
         return dstLen + sizeof(size_t);
     }
 
