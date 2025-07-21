c65c8e5250e58892e48e384e274d2d532d0b3664
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2025-07-21 14:36:57 -0400

    changes

--- include/SZ3/api/impl/SZDispatcher.hpp
+++ include/SZ3/api/impl/SZDispatcher.hpp
@@ -52,8 +52,8 @@ size_t SZ_compress_dispatcher(Config &conf, const T *data, uchar *cmpData, size_
     if (conf.cmprAlgo == ALGO_LOSSLESS || !isCmpCapSufficient) {
         conf.cmprAlgo = ALGO_LOSSLESS;
         auto zstd = Lossless_zstd();
-        auto zstdCmpCap = ZSTD_compressBound(conf.num * sizeof(T));
-        auto zstdCmpData = static_cast<uchar *>(malloc(zstdCmpCap));
+        // auto zstdCmpCap = ZSTD_compressBound(conf.num * sizeof(T));
+        // auto zstdCmpData = static_cast<uchar *>(malloc(zstdCmpCap));
         std::cout<<"ZSTD compression"<<std::endl;
          return zstd.compress(reinterpret_cast<const uchar *>(data), conf.num * sizeof(T), cmpData, cmpCap);
         //return zstd.compress(reinterpret_cast<const uchar *>(data), conf.num * sizeof(T), zstdCmpData, zstdCmpCap);
@@ -62,7 +62,7 @@ size_t SZ_compress_dispatcher(Config &conf, const T *data, uchar *cmpData, size_
     //if lossy compression ratio < 3, test if lossless only mode has a better ratio than lossy
     if (conf.num * sizeof(T) / 1.0 / cmpSize < 3) {
         auto zstd = Lossless_zstd();
-        auto zstdCmpCap = ZSTD_compressBound(conf.num * sizeof(T));
+        auto zstdCmpCap = ZSTD_compressBound(conf.num * sizeof(T))+sizeof(size_t);
         auto zstdCmpData = static_cast<uchar *>(malloc(zstdCmpCap));
         std::cout<<"zstdCmpCap>>>"<<zstdCmpCap<<std::endl;
         std::cout<<"src data size"<<ZSTD_compressBound(conf.num * sizeof(T))<<std::endl;
--- tools/sz3/sz3_smoke_test.cpp
+++ tools/sz3/sz3_smoke_test.cpp
@@ -61,43 +61,58 @@ char *compress_data(T *uncompressedData, size_t size, size_t &cmpSize) {
     char *cmpData = SZ_compress(conf, uncompressedData, cmpSize);
     return cmpData;
 }
+
+template <typename T>
+void decompress_data(T *uncompressedData, size_t size, size_t cmpSize, char *cmpData, T *dec_data_p) {
+    SZ3::Config conf({size});
+    conf.cmprAlgo = SZ3::ALGO_INTERP_LORENZO;
+    conf.errorBoundMode = SZ3::EB_ABS;
+    conf.absErrorBound = 1e-3;
+    SZ_decompress(conf, cmpData, cmpSize, dec_data_p);
+    // return cmpData;
+}
+
 int main(int argc, char **argv) {
-    size_t size = 51;
+    size_t size = 41;
     SZ3::Config conf({size});
     conf.cmprAlgo = SZ3::ALGO_INTERP_LORENZO;
     conf.errorBoundMode = SZ3::EB_ABS;  // refer to def.hpp for all supported error bound mode
     conf.absErrorBound = 1E-3;          // absolute error bound 1e-3
 
     std::vector<double> input_data = {
-        -0.000557246, -1.91871e-05, 3.64865e-05, -0.000174678, -4.16519e-05, 2.09643e-06, 3.49307e-06, -1.05011e-05,
-        -0.000472014, -3.93203e-06, 0.000152117, 0.00016113, -2.02534e-05, -0.000228394, 0.000168178, -1.04183e-05,
-        9.51545e-05, 0.000136021, 6.12364e-05, 0.00123887, -1.95963e-05, -1.86062e-05, -2.23122e-05, -0.00133366,
-        -2.67941e-05, -6.13811e-05, 0.0020968, -0.00257579, 0.000133028, 0.000125964, 7.11653e-05, 3.86562e-05,
-        0.00271601, -2.6028e-05, 0.000115805, -3.44433e-05, -6.6229e-05, -3.97414e-05, -6.85452e-05, -5.14098e-05,
-        -3.44433e-05, -6.6229e-05, -3.97414e-05, -6.85452e-05, -5.14098e-05,
-        -3.44433e-05, -6.6229e-05, -3.97414e-05, -6.85452e-05, -5.14098e-05,
-        0.00628354};
+        -0.000557246, -1.91871e-05, 3.64865e-05,  -0.000174678, -4.16519e-05, 2.09643e-06,  3.49307e-06,  -1.05011e-05,
+        -0.000472014, -3.93203e-06, 0.000152117,  0.00016113,   -2.02534e-05, -0.000228394, 0.000168178,  -1.04183e-05,
+        9.51545e-05,  0.000136021,  6.12364e-05,  0.00123887,   -1.95963e-05, -1.86062e-05, -2.23122e-05, -0.00133366,
+        -2.67941e-05, -6.13811e-05, 0.0020968,    -0.00257579,  0.000133028,  0.000125964,  7.11653e-05,  3.86562e-05,
+        0.00271601,   -2.6028e-05,  0.000115805,  -3.44433e-05, -6.6229e-05,  -3.97414e-05, -6.85452e-05, -5.14098e-05,
+        -3.44433e-05, -6.6229e-05,  -3.97414e-05, -6.85452e-05, -5.14098e-05, -3.44433e-05, -6.6229e-05,  -3.97414e-05,
+        -6.85452e-05, -5.14098e-05, -3.44433e-05, -6.6229e-05,  -3.97414e-05, -6.85452e-05, -5.14098e-05, 0.00628354};
     //   for (size_t i = 0; i < conf.num; i++) {
     //     input_data[i] = static_cast<double>(i * 1000UL);
     // }
     std::cout << "Vector size: " << input_data.size() << std::endl;
     size_t cmpSize;
-    // char *cmpData = SZ_compress(conf, input_data.data(), cmpSize);
+    char *cmpData = SZ_compress(conf, input_data.data(), cmpSize);
 
-  //  char *cmpData = compress_data<double>(input_data.data(), size, cmpSize);
+    char *cmpData = compress_data<double>(input_data.data(), size, cmpSize);
 
+    auto dec_data_p = dec_data.data();
+    SZ_decompress(conf, cmpData, cmpSize, dec_data_p);
+    std::cout << "Compressed" << cmpSize << std::endl;
+    // //    printf("%lu ", conf.num);
+    // ////////////////
+    // auto zstd = SZ3::Lossless_zstd();
+    // auto zstdCmpCap = ZSTD_compressBound(conf.num * sizeof(double)) + sizeof(size_t);
+    // auto zstdCmpData = static_cast<uchar *>(malloc(zstdCmpCap));
+    // size_t zstdCmpSize = zstd.compress(reinterpret_cast<const uchar *>(input_data.data()), conf.num * sizeof(double),
+    //                                    zstdCmpData, zstdCmpCap);
+    // std::cout << "loss Compressed" << zstdCmpSize << std::endl;
+
+    // std::vector<T> dec_data(datasize);
     // auto dec_data_p = dec_data.data();
-    // SZ_decompress(conf, cmpData, cmpSize, dec_data_p);
-    //std::cout << "Compressed" << cmpSize << std::endl;
-    //    printf("%lu ", conf.num);
-    ////////////////
-    auto zstd = SZ3::Lossless_zstd();
-    auto zstdCmpCap = ZSTD_compressBound(conf.num * sizeof(double))+sizeof(size_t);
-    auto zstdCmpData = static_cast<uchar *>(malloc(zstdCmpCap));
-    size_t zstdCmpSize =
-        zstd.compress(reinterpret_cast<const uchar *>(input_data.data()), conf.num * sizeof(double), zstdCmpData, zstdCmpCap);
-std::cout << "loss Compressed" << zstdCmpSize << std::endl;
+    // decompress_data(&buf[start * block_size], datasize, cmpSize, cmpData, dec_data_p);
+
     ///////////////
-   // delete[] cmpData;
+    delete[] cmpData;
     return 0;
 }
